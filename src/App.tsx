9ecd0a02ffdf53a337b5b087350a326e53ebcc41
Sushane Yadav <78971334+shaneydv@users.noreply.github.com>
2023-10-26 12:13:23 +0530

    search books page added

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { HomePage } from "./layouts/HomePage/HomePage";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
 
 export const App = () => {
   return (
     <>
       <Navbar />
       <HomePage />
+      <SearchBooksPage />
       <Footer />
     </>
   );
